9d855b0074c016d5f571ec2145ca0d5b77a4fb71
Ben Chapman <bchapman@engineyard.com>
2013-05-27 16:58:57 +0100

    Created PHP Environment Variable recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,2 +1,5 @@
 # Downgrade to PHP 5.3
 #include_recipe "php::php53"
+
+# Add custom environment variables to PHP
+#include_recipe "php::environment_variables"
---
+++ cookbooks/php/recipes/environment_variables.rb
@@ -0,0 +1,16 @@
+ey_cloud_report "php53" do
+  message "Adding PHP environment variables"
+end
+
+if ['app_master', 'app', 'util', 'solo'].include?(node[:instance_role])
+  node[:applications].each do |app, data|
+    template "/data/#{app}/shared/config/env.custom" do
+      source 'env.custom'
+      owner node[:owner_name]
+      group node[:owner_name]
+      mode 0655
+      backup 0
+      action :create
+    end
+  end
+end
